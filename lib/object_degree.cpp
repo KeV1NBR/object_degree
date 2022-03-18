be253ec467e67e09524f47978e5c2d36ad316bd9
kevin wu <kevin6991517@icloud.com>
2022-03-18 16:31:47 +0800

    add contour compare

--- lib/object_degree.cpp
+++ lib/object_degree.cpp
@@ -1,139 +1,103 @@
-#include<iostream>
+#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <vector>
+
+#include "opencv2/imgproc.hpp"
 #define OPENCV
 #include <yolo_v2_class.hpp>
 
-#include "object_degree.hpp"
+#include "object_degree.h"
 
 using namespace cv;
 using namespace std;
 
-Detector_deg::Detector_deg(string cfg, string weight): Detector(cfg, weight)
-{
-
+bool areaComp(const vector<Point>& lhs, const vector<Point>& rhs) {
+    return (contourArea(lhs) < contourArea(rhs)) ? true : false;
 }
 
-Detector_deg::~Detector_deg()
-{
+Detector_deg::Detector_deg(string cfg, string weight) : Detector(cfg, weight) {}
 
-}
-vector<bbox_t_deg> Detector_deg::detectWithDeg(Mat img, float thresh, bool use_mean, int thres1, int thres2, int rho, int theta, int lineThres, int rhoScale, int thetaScale)
-{
-    vector<bbox_t> darknet_predict;
-    vector<bbox_t_deg> predict;
-    
-    darknet_predict = detect(img, thresh,use_mean);
+Detector_deg::~Detector_deg() {}
+// bbox_t_deg Detector_deg::detectWithDeg(Mat img) {
+//    vector<bbox_t> darknet_predict;
+//    vector<bbox_t_deg> predict;
+//
+//    darknet_predict = detect(img, 0.5);
+//
+//    unsigned int i = 0;
+//    for (i = 0; i < predict.size(); i++) {
+//        predict[i].x = darknet_predict[i].x;
+//        predict[i].y = darknet_predict[i].y;
+//        predict[i].w = darknet_predict[i].w;
+//        predict[i].h = darknet_predict[i].h;
+//        predict[i].prob = darknet_predict[i].prob;
+//        predict[i].obj_id = darknet_predict[i].obj_id;
+//        predict[i].track_id = darknet_predict[i].track_id;
+//        predict[i].frames_counter = darknet_predict[i].frames_counter;
+//    }
+//
+//    i = 0;
+//    for (auto& item : darknet_predict) {
+//        Rect bbox(item.x, item.y, item.w, item.h);
+//        bbox.width = min(640 - bbox.x, bbox.width);
+//        bbox.height = min(480 - bbox.y, bbox.height);
+//        Mat crop = img(bbox).clone();
+//        Mat gray;
+//
+//        cvtColor(crop, gray, COLOR_BGR2GRAY);
+//
+//        cvtColor(canny, cannyBGR, COLOR_GRAY2BGR);
+//        drawLines(crop, lines);
+//
+//        Mat combine = Mat::zeros(crop.rows + 20, crop.cols * 2, CV_8UC3);
+//        crop.copyTo(combine(Rect(Point(), crop.size())));
+//        cannyBGR.copyTo(combine(Rect(Point(canny.cols, 0), canny.size())));
+//        predict[i].degree = calcDeg(lines);
+//        putText(combine, std::to_string(predict[i].degree),
+//                Point(0, crop.rows + 18), 0, 0.5, Scalar(255, 255, 255));
+//
+//        // imshow("Canny" + std::to_string(i), combine);
+//        i++;
+//    }
+//    return predict;
+//}
 
-    unsigned int i = 0;
-    for(i=0; i<predict.size();i++)
-    {
-        predict[i].x = darknet_predict[i].x;
-        predict[i].y = darknet_predict[i].y;
-        predict[i].w = darknet_predict[i].w;
-        predict[i].h = darknet_predict[i].h;
-        predict[i].prob = darknet_predict[i].prob;
-        predict[i].obj_id = darknet_predict[i].obj_id;
-        predict[i].track_id = darknet_predict[i].track_id;
-        predict[i].frames_counter = darknet_predict[i].frames_counter;
-    }
-
-    i = 0;
-    for(auto& item: darknet_predict)
-    {
-        Rect bbox(item.x, item.y, item.w, item.h);
-        bbox.width = min(640-bbox.x, bbox.width);
-        bbox.height = min(480-bbox.y, bbox.height);
-        Mat crop = img(bbox).clone();
-        Mat gray;
-        cvtColor(crop, gray, COLOR_BGR2GRAY);
-        Mat canny;
-        Mat cannyBGR;
-        vector<Vec2f> lines;
-        Canny(gray, canny, thres1, thres2);
-        HoughLines(canny, lines, (double)rho / rhoScale, (double)theta / thetaScale, lineThres);
-        cvtColor(canny, cannyBGR, COLOR_GRAY2BGR);
-        drawLines(crop, lines);
-        Mat combine = Mat::zeros(crop.rows + 20, crop.cols * 2, CV_8UC3);
-        crop.copyTo(combine(Rect(Point(), crop.size())));
-        cannyBGR.copyTo(combine(Rect(Point(canny.cols, 0), canny.size())));
-        predict[i].degree = calcDeg(lines);
-        putText(combine, std::to_string(predict[i].degree), Point(0, crop.rows + 18), 0, 0.5, Scalar(255, 255, 255));
-
-        //imshow("Canny" + std::to_string(i), combine);
-        i++;
-    }
-    return predict;
-}
-
-double Detector_deg::detectSingleWithDeg(cv::Mat crop, int thres1, int thres2, int rho, int theta, int lineThres, int rhoScale, int thetaScale)
-{
+double Detector_deg::calcDeg(Mat& crop) {
+    // Convert image to grayscale
     Mat gray;
     cvtColor(crop, gray, COLOR_BGR2GRAY);
-    Mat canny;
-    Mat cannyBGR;
-    vector<Vec2f> lines;
-    double degree;
+    // Convert image to binary
+    Mat bw;
+    threshold(gray, bw, 50, 255, THRESH_BINARY | THRESH_OTSU);
+    // Find all the contours in the thresholded image
+    vector<vector<Point> > contours;
+    findContours(bw, contours, RETR_LIST, CHAIN_APPROX_NONE);
 
-    Canny(gray, canny, thres1, thres2);
-    HoughLines(canny, lines, (double)rho / rhoScale, (double)theta / thetaScale, lineThres);
-    cvtColor(canny, cannyBGR, COLOR_GRAY2BGR);
-    drawLines(crop, lines);
-    Mat combine = Mat::zeros(crop.rows + 20, crop.cols * 2, CV_8UC3);
-    crop.copyTo(combine(Rect(Point(), crop.size())));
-    cannyBGR.copyTo(combine(Rect(Point(canny.cols, 0), canny.size())));
-    degree = calcDeg(lines);
-    putText(combine, std::to_string(degree), Point(0, crop.rows + 18), 0, 0.5, Scalar(255, 255, 255));
+    vector<Point> pts;
 
-    //imshow("Canny", combine);
+    for (auto c : contours) pts = areaComp(pts, c) ? c : pts;
 
-    return degree;
-}
+    // Construct a buffer used by the pca analysis
+    int sz = static_cast<int>(pts.size());
+    Mat data_pts = Mat(sz, 2, CV_64F);
 
-
-double Detector_deg::calcDeg(const std::vector<cv::Vec2f>& lines)
-{
-    double ret = -999;
-    if(lines.size() > 0) 
-    {
-        ret = 0;
-        std::vector<double> degs;
-        for(const cv::Vec2f& line: lines) 
-        {
-            degs.push_back(line[1] * 2.0);
-        }
-        double s = 0, c = 0;
-        for(double& d: degs) 
-        {
-            s += sin(d);
-            c += cos(d);
-        }
-        s /= degs.size();
-        c /= degs.size();
-        
-        ret = atan2(s, c);
-        ret /= 2;
-        ret = ret * 180.0 / CV_PI;
+    for (int i = 0; i < data_pts.rows; i++) {
+        data_pts.at<double>(i, 0) = pts[i].x;
+        data_pts.at<double>(i, 1) = pts[i].y;
     }
-    return ret;
-}
-void Detector_deg::drawLines(cv::Mat &input, const std::vector<cv::Vec2f> &lines)
-{
-    for(size_t i=0; i<lines.size(); i++)
-    {
-        float r = lines[i][0];
-        float theta = lines[i][1];
-        if(theta<CV_PI/4.0 || theta>3*CV_PI/4.0)
-        {
-           cv::Point pt1(r/cos(theta),0);
-            cv::Point pt2((r-input.rows*sin(theta))/cos(theta), input.rows);
-            line(input, pt1, pt2, cv::Scalar(255,0,0), 1);
-        } 
-        else 
-        {
-            cv::Point pt1(0,r/sin(theta));
-            cv::Point pt2(input.cols, (r-input.cols*cos(theta))/sin(theta));
-            cv::line(input, pt1, pt2, cv::Scalar(255,0,0), 1);
-        }
-    }
-}
 
+    // Perform PCA analysis
+    PCA pca_analysis(data_pts, Mat(), PCA::DATA_AS_ROW);
+
+    // Store the eigenvalues and eigenvectors
+    vector<Point2d> eigen_vecs(2);
+    vector<double> eigen_val(2);
+
+    for (int i = 0; i < 2; i++) {
+        eigen_vecs[i] = Point2d(pca_analysis.eigenvectors.at<double>(i, 0),
+                                pca_analysis.eigenvectors.at<double>(i, 1));
+        eigen_val[i] = pca_analysis.eigenvalues.at<double>(i);
+    }
+
+    return atan2(eigen_vecs[0].y, eigen_vecs[0].x);  // orientation in radians;
+}
---
+++ lib/object_degree.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#define OPENCV
+#include <yolo_v2_class.hpp>
+
+struct bbox_t_deg {
+    unsigned int x, y, w,
+        h;  // (x,y) - top-left corner, (w, h) - width & height of bounded box
+    float prob;  // confidence - probability that the object was found correctly
+    unsigned int obj_id;    // class of object - from range [0, classes-1]
+    unsigned int track_id;  // tracking id for video (0 - untracked, 1 - inf -
+                            // tracked object)
+    unsigned int
+        frames_counter;  // counter of frames on which the object was detected
+
+    double degree;
+};
+
+class Detector_deg : public Detector {
+   public:
+    Detector_deg(std::string cfg, std::string weight);
+    ~Detector_deg();
+
+    std::vector<bbox_t_deg> detectWithDeg(cv::Mat img);
+
+    bbox_t_deg detectSingleWithDeg(cv::Mat img, uint objID);
+
+   protected:
+    double calcDeg(cv::Mat& crop);
+};
